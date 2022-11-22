82ec2f3a64945ac7b3657e330eb0f0e67c9169af
Canaan <reakak1@gmail.com>
2022-11-22 05:00:11 -0800

    Create first_contact.rs

---
+++ src/first_contact.rs
@@ -0,0 +1,22 @@
+
+// the plan is to contact a server to get some info 
+
+#[tokio::main]
+async fn first_contact() {
+    
+  let greeting = greet_fast_alien().await;  
+    println!("Greetings = {:#?}", greeting);
+}
+
+async fn greet_fast_alien() -> Result<String, Box<dyn std::error::Error>>{
+    let client = reqwest::Client::new();
+    let body = client.get("http://localhost:8989")
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{}", body);
+    //{"Greetings ~! ":"from your lovely neighbor fast alien ~!"}
+    // deserialize maybe ? 
+    Ok(body)
+}
